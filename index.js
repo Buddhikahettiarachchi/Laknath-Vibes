e337376d1a985d96783d501e2a40bd471a36e31c
Buddhika Hettiarachchi <hpradeepbuddhika@gmail.com>
2020-08-13 12:56:32 +0530

    made mobile website partially

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+
+//toggle button function
+
 $(document).ready(function () {
-  $("#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4").click(function () {
+  $("#nav-icon1").click(function () {
     $(this).toggleClass("open");
+    console.log("btn clicked")
+    $( "#navigation" ).toggleClass("toggle-nav-bar-color");
+    $(".banner-overlay-hidden").toggleClass("banner-overlay-toggle");
   });
 });
-$(window).scroll(function () {
-  if ($(window).scrollTop() >= '250') {
-    console.log("from top");
-  }
-});
-console.log("Width: " + screen.width + ": Height: " + screen.height);
+
+//scroll function
+
+// $(window).scroll(function () {
+//   if ($(window).scrollTop() > '250' && $(window).scrollTop() < '300') {
+//     console.log($(window).scrollTop());
+//     $( "#navigation" ).toggleClass("hide-navigation");
+//   }
+// });
+// console.log("Width: " + screen.width + ": Height: " + screen.height);
